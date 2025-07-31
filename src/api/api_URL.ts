5f0f1a7b63bd0dbf248e2096043e9e5cf19c7748
chonuboy <knanthakumar12@gmail.com>
2025-08-01 04:27:33 +0530

    v0.0.3

--- src/api/api_URL.ts
+++ src/api/api_URL.ts
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:9090/";
+// export const API_URL = "http://localhost:9090/";
 // export const swaggerurl = "http://fee7-110-235-232-108.ngrok-free.app"
-// export const API_URL = "http://34.0.4.159:9090/";
+export const API_URL = "http://34.0.4.159:9090/";
